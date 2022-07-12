5ffdeea4d5e8f345e4fd2c74f687d1f64f38c6d3
claudiahidalgoramirez <108893603+claudiahidalgoramirez@users.noreply.github.com>
2022-07-12 01:08:53 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,23 @@
+const btn  = document.querySelector('button');
+const popupCaja = document.querySelector('.popup-caja');
+const btnClose = document.querySelector('.popup-close');
+
+btn.addEventListener('click', function(){
+    //console.log('funcionaaaa');
+    popupCaja.classList.add('mostrarCaja');
+})
+btnClose.addEventListener('click',()=>{
+    popupCaja.classList.remove('mostrarCaja');
+});
+window.addEventListener('keyup', e=>{
+    console.log(e);
+    if(e.key==='Escape'){
+        popupCaja.classList.remove('mostrarCaja');
+    }
+})
+
+popupCaja.addEventListener('click',function(e){
+    if(e.target.classList.contains('popup-caja')){
+        popupCaja.classList.remove('mostrarCaja');
+    }
+})
